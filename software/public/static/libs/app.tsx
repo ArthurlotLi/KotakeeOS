e402d94a979efcf745bee01db6936acfc7ba99a6
arthur <ali64@ucsc.edu>
2021-05-25 13:14:15 -0700

    Added basic clock functionality with timer updates per second.

--- software/public/static/libs/app.tsx
+++ software/public/static/libs/app.tsx
@@ -1,7 +1,8 @@
 /*
   app.tsx
+
   Primary script for serving front-facing dashboard functionality. 
-  Typescript, so should be transpiled into Javascript before being
+  Typescript, so it should be transpiled into Javascript before being
   executed. 
 */
 
@@ -11,18 +12,59 @@ var ReactDOM = require('react-dom');
 export class App extends React.Component {
   constructor(){
     super();
-    this.state = {};
-    // This is where you'd put this.function = this.function.bind(this);
+    this.state = {
+      currentHoursMinutes: null,
+      currentSeconds: null,
+      currentDayMonthYear: null,
+    };
+    this.updateTime = this.updateTime.bind(this);
   }
 
+  // Modify date state variables whenever called (timer-linked. )
+  updateTime(){
+    var date = new Date();
+    var time = date.toLocaleTimeString(navigator.language, {
+      hour: '2-digit',
+      minute:'2-digit',
+      second:'2-digit',
+    });
+    var splitTime = time.split(":");
+    var hours = splitTime[0];
+    var minutes = splitTime[1];
+    var seconds = splitTime[2];
+    if(hours.charAt(0) == '0'){
+      // Strip leading zero from hours if present.
+      hours = hours.charAt(1);
+    }
+    var currentHoursMinutes =  hours + ":" + minutes;
+    var currentSeconds = seconds;
+
+    var currentDay = date.getDate().toString();
+    var currentMonth = date.toLocaleString('default', {month: 'long'});
+    var curentYear = date.getFullYear().toString();
+    var currentDayMonthYear = currentDay + " " + currentMonth + " " + curentYear;
+
+    this.setState({
+      currentHoursMinutes: currentHoursMinutes,
+      currentSeconds: currentSeconds,
+      currentDayMonthYear: currentDayMonthYear,
+    });
+  }
+
+  // Executed only once upon startup.
   async componentDidMount(){
-    // Add whatever you'd like here. 
+    // Start the clock and the interval to update it. 
+    this.updateTime();
+    setInterval(this.updateTime, 1000);
   }
 
   render() {
     return(
       <div>
-        <h1>Hello there!</h1>
+        <div id="App-clock">
+          <h1>{this.state.currentHoursMinutes}<span> {this.state.currentSeconds}</span></h1>
+          <h3>{this.state.currentDayMonthYear}</h3>
+        </div>
       </div>
     );
   }
--- software/public/static/libs/dist/app-bundle.js
+++ software/public/static/libs/dist/app-bundle.js
@@ -29822,8 +29822,9 @@ if (false) {} else {
 
 /*
   app.tsx
+
   Primary script for serving front-facing dashboard functionality.
-  Typescript, so should be transpiled into Javascript before being
+  Typescript, so it should be transpiled into Javascript before being
   executed.
 */
 var __extends = (this && this.__extends) || (function () {
@@ -29841,6 +29842,42 @@ var __extends = (this && this.__extends) || (function () {
         d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
     };
 })();
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
+var __generator = (this && this.__generator) || function (thisArg, body) {
+    var _ = { label: 0, sent: function() { if (t[0] & 1) throw t[1]; return t[1]; }, trys: [], ops: [] }, f, y, t, g;
+    return g = { next: verb(0), "throw": verb(1), "return": verb(2) }, typeof Symbol === "function" && (g[Symbol.iterator] = function() { return this; }), g;
+    function verb(n) { return function (v) { return step([n, v]); }; }
+    function step(op) {
+        if (f) throw new TypeError("Generator is already executing.");
+        while (_) try {
+            if (f = 1, y && (t = op[0] & 2 ? y["return"] : op[0] ? y["throw"] || ((t = y["return"]) && t.call(y), 0) : y.next) && !(t = t.call(y, op[1])).done) return t;
+            if (y = 0, t) op = [op[0] & 2, t.value];
+            switch (op[0]) {
+                case 0: case 1: t = op; break;
+                case 4: _.label++; return { value: op[1], done: false };
+                case 5: _.label++; y = op[1]; op = [0]; continue;
+                case 7: op = _.ops.pop(); _.trys.pop(); continue;
+                default:
+                    if (!(t = _.trys, t = t.length > 0 && t[t.length - 1]) && (op[0] === 6 || op[0] === 2)) { _ = 0; continue; }
+                    if (op[0] === 3 && (!t || (op[1] > t[0] && op[1] < t[3]))) { _.label = op[1]; break; }
+                    if (op[0] === 6 && _.label < t[1]) { _.label = t[1]; t = op; break; }
+                    if (t && _.label < t[2]) { _.label = t[2]; _.ops.push(op); break; }
+                    if (t[2]) _.ops.pop();
+                    _.trys.pop(); continue;
+            }
+            op = body.call(thisArg, _);
+        } catch (e) { op = [6, e]; y = 0; } finally { f = t = 0; }
+        if (op[0] & 5) throw op[1]; return { value: op[0] ? op[1] : void 0, done: true };
+    }
+};
 Object.defineProperty(exports, "__esModule", ({ value: true }));
 exports.App = void 0;
 var React = __webpack_require__(/*! react */ "./node_modules/react/index.js");
@@ -29848,10 +29885,63 @@ var ReactDOM = __webpack_require__(/*! react-dom */ "./node_modules/react-dom/in
 var App = /** @class */ (function (_super) {
     __extends(App, _super);
     function App() {
-        return _super !== null && _super.apply(this, arguments) || this;
+        var _this = _super.call(this) || this;
+        _this.state = {
+            currentHoursMinutes: null,
+            currentSeconds: null,
+            currentDayMonthYear: null,
+        };
+        _this.updateTime = _this.updateTime.bind(_this);
+        return _this;
     }
+    // Modify date state variables whenever called (timer-linked. )
+    App.prototype.updateTime = function () {
+        var date = new Date();
+        var time = date.toLocaleTimeString(navigator.language, {
+            hour: '2-digit',
+            minute: '2-digit',
+            second: '2-digit',
+        });
+        var splitTime = time.split(":");
+        var hours = splitTime[0];
+        var minutes = splitTime[1];
+        var seconds = splitTime[2];
+        if (hours.charAt(0) == '0') {
+            // Strip leading zero from hours if present.
+            hours = hours.charAt(1);
+        }
+        var currentHoursMinutes = hours + ":" + minutes;
+        var currentSeconds = seconds;
+        var currentDay = date.getDate().toString();
+        var currentMonth = date.toLocaleString('default', { month: 'long' });
+        var curentYear = date.getFullYear().toString();
+        var currentDayMonthYear = currentDay + " " + currentMonth + " " + curentYear;
+        this.setState({
+            currentHoursMinutes: currentHoursMinutes,
+            currentSeconds: currentSeconds,
+            currentDayMonthYear: currentDayMonthYear,
+        });
+    };
+    // Executed only once upon startup.
+    App.prototype.componentDidMount = function () {
+        return __awaiter(this, void 0, void 0, function () {
+            return __generator(this, function (_a) {
+                // Start the clock and the interval to update it. 
+                this.updateTime();
+                setInterval(this.updateTime, 1000);
+                return [2 /*return*/];
+            });
+        });
+    };
     App.prototype.render = function () {
-        return (React.createElement("h1", null, "Hello!"));
+        return (React.createElement("div", null,
+            React.createElement("div", { id: "App-clock" },
+                React.createElement("h1", null,
+                    this.state.currentHoursMinutes,
+                    React.createElement("span", null,
+                        " ",
+                        this.state.currentSeconds)),
+                React.createElement("h3", null, this.state.currentDayMonthYear))));
     };
     return App;
 }(React.Component));
