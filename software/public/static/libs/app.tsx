560c5f7fb47aa6a05566069ded62a14b86f32c0e
arthur <ali64@ucsc.edu>
2021-05-25 17:38:28 -0700

    Implement Open Weather Map functionality

--- software/public/static/libs/app.tsx
+++ software/public/static/libs/app.tsx
@@ -9,12 +9,16 @@
 var React = require('react');
 var ReactDOM = require('react-dom');
 
+const openweathermapZipCode = "95051"
+const openweathermapApiKey = "47ad011b1eb24c37b31f2805da701cc4";
+
 export class App extends React.Component {
   constructor(){
     super();
 
     // Interval handles to clean up.
     this.updateTimeInverval = null;
+    this.updateWeatherInterval = null;
     
     // State
     this.state = {
@@ -22,13 +26,17 @@ export class App extends React.Component {
       currentSeconds: null,
       currentAmPm: null,
       currentDayMonthYear: null,
+      currentWeatherMain: null,
+      currentWeatherMinMax: null,
+      currentWeatherFeelsLike: null,
     };
 
     // Binding functions to "this"
     this.updateTime = this.updateTime.bind(this);
+    this.updateWeather = this.updateWeather.bind(this);
   }
 
-  // Modify date state variables whenever called (timer-linked. )
+  // Modify date state variables whenever called (timer-linked.)
   updateTime(){
     var date = new Date();
     var time = date.toLocaleTimeString(navigator.language, {
@@ -63,6 +71,57 @@ export class App extends React.Component {
     });
   }
 
+  // Modify weather state variables whenever called (timer-linked)
+  async updateWeather(){
+    var apiResponse = null;
+    var startTime, endTime; // We report in debug the api time.
+    try{
+      startTime = new Date();
+      apiResponse = await fetch("http://api.openweathermap.org/data/2.5/weather?zip="+openweathermapZipCode+"&units=imperial&appid=" + openweathermapApiKey);
+      endTime = new Date();
+      var timeDiff = endTime - startTime;
+      console.log("DEBUG: Open Weather Map API call returned in " + timeDiff/1000 + " seconds.");
+    }
+    catch(error){
+      console.log("ERROR: Open Weather Map API call failed!");
+    }
+    if(apiResponse.status == 200){
+      var receivedData = await apiResponse.json();
+  
+      console.log("DEBUG: Received Open Weather Map API data:");
+      console.log(receivedData);
+
+      // Given open weather map JSON data, parse it. See example: 
+      // https://openweathermap.org/current#zip
+      var weatherMain = receivedData.weather[0].main; // "Clear"
+      var weatherDesc = receivedData.weather[0].description; // "clear sky"
+      var mainTemp = receivedData.main.temp;
+      var mainFeels_like = receivedData.main.feels_like; 
+      var mainTemp_min = receivedData.main.temp_min;
+      var mainTemp_max = receivedData.main.temp_max; 
+      var mainPressure = receivedData.main.pressure; // "1023"
+      var mainHumidity = receivedData.main.humidity; // "100"
+      var visibility = receivedData.visibility; // "16093"
+      var windSpeed = receivedData.wind.speed; // "1.5"
+      var windDeg = receivedData.wind.deg; // "350"
+      var dt = receivedData.dt; // "1560350645"
+      var sysSunrise = receivedData.sys.sunrise; // "1560343627"
+      var sysSunset = receivedData.sys.sunset; // "1560396563"
+
+      var currentWeatherMain = parseInt(mainTemp).toFixed(0) + " F - " + weatherMain;
+      var currentWeatherMinMax = parseInt(mainTemp_min).toFixed(0) + " F | " + parseInt(mainTemp_max).toFixed(0) + " F";
+      var currentWeatherFeelsLike = "Feels Like: " + parseInt(mainFeels_like).toFixed(0) + " F";
+      this.setState({
+        currentWeatherMain: currentWeatherMain,
+        currentWeatherMinMax: currentWeatherMinMax,
+        currentWeatherFeelsLike: currentWeatherFeelsLike,
+      });
+    }
+    else{
+      console.log("WARNING: Open Weather Map API call returned with status " + apiResponse.status + ".");
+    }
+  }
+
   moduleLightingBedroom(){
     console.log("TODO: Bedroom lighting module activated.");
   }
@@ -77,9 +136,13 @@ export class App extends React.Component {
 
   // Executed only once upon startup.
   componentDidMount(){
-    // Start the clock and the interval to update it. 
+    // Start the clock and the interval to update it every second.
     this.updateTime();
     this.updateTimeInverval = setInterval(this.updateTime, 1000);
+
+    // Query the weather and start the interval to update it (every 60 minutes).
+    this.updateWeather();
+    this.updateWeatherInterval = setInterval(this.updateWeather, 3600000);
   }
 
   // Executed upon close.
@@ -91,7 +154,7 @@ export class App extends React.Component {
     return(
       <div>
         <div id="app-location">
-          Location. 
+          Santa Clara, CA
         </div>
 
         <div id="app-clock">
@@ -104,7 +167,9 @@ export class App extends React.Component {
         </div>
 
         <div id="app-weather">
-          Weather. 
+          <div id="app-weather-main">{this.state.currentWeatherMain}</div>
+          <div id="app-weather-minMax">{this.state.currentWeatherMinMax}</div>
+          <div id="app-weather-feelsLike">{this.state.currentWeatherFeelsLike}</div>
         </div>
 
         <div id="app-modules">
--- software/public/static/libs/dist/app-bundle.js
+++ software/public/static/libs/dist/app-bundle.js
@@ -29842,28 +29842,71 @@ var __extends = (this && this.__extends) || (function () {
         d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
     };
 })();
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
+var __generator = (this && this.__generator) || function (thisArg, body) {
+    var _ = { label: 0, sent: function() { if (t[0] & 1) throw t[1]; return t[1]; }, trys: [], ops: [] }, f, y, t, g;
+    return g = { next: verb(0), "throw": verb(1), "return": verb(2) }, typeof Symbol === "function" && (g[Symbol.iterator] = function() { return this; }), g;
+    function verb(n) { return function (v) { return step([n, v]); }; }
+    function step(op) {
+        if (f) throw new TypeError("Generator is already executing.");
+        while (_) try {
+            if (f = 1, y && (t = op[0] & 2 ? y["return"] : op[0] ? y["throw"] || ((t = y["return"]) && t.call(y), 0) : y.next) && !(t = t.call(y, op[1])).done) return t;
+            if (y = 0, t) op = [op[0] & 2, t.value];
+            switch (op[0]) {
+                case 0: case 1: t = op; break;
+                case 4: _.label++; return { value: op[1], done: false };
+                case 5: _.label++; y = op[1]; op = [0]; continue;
+                case 7: op = _.ops.pop(); _.trys.pop(); continue;
+                default:
+                    if (!(t = _.trys, t = t.length > 0 && t[t.length - 1]) && (op[0] === 6 || op[0] === 2)) { _ = 0; continue; }
+                    if (op[0] === 3 && (!t || (op[1] > t[0] && op[1] < t[3]))) { _.label = op[1]; break; }
+                    if (op[0] === 6 && _.label < t[1]) { _.label = t[1]; t = op; break; }
+                    if (t && _.label < t[2]) { _.label = t[2]; _.ops.push(op); break; }
+                    if (t[2]) _.ops.pop();
+                    _.trys.pop(); continue;
+            }
+            op = body.call(thisArg, _);
+        } catch (e) { op = [6, e]; y = 0; } finally { f = t = 0; }
+        if (op[0] & 5) throw op[1]; return { value: op[0] ? op[1] : void 0, done: true };
+    }
+};
 Object.defineProperty(exports, "__esModule", ({ value: true }));
 exports.App = void 0;
 var React = __webpack_require__(/*! react */ "./node_modules/react/index.js");
 var ReactDOM = __webpack_require__(/*! react-dom */ "./node_modules/react-dom/index.js");
+var openweathermapZipCode = "95051";
+var openweathermapApiKey = "47ad011b1eb24c37b31f2805da701cc4";
 var App = /** @class */ (function (_super) {
     __extends(App, _super);
     function App() {
         var _this = _super.call(this) || this;
         // Interval handles to clean up.
         _this.updateTimeInverval = null;
+        _this.updateWeatherInterval = null;
         // State
         _this.state = {
             currentHoursMinutes: null,
             currentSeconds: null,
             currentAmPm: null,
             currentDayMonthYear: null,
+            currentWeatherMain: null,
+            currentWeatherMinMax: null,
+            currentWeatherFeelsLike: null,
         };
         // Binding functions to "this"
         _this.updateTime = _this.updateTime.bind(_this);
+        _this.updateWeather = _this.updateWeather.bind(_this);
         return _this;
     }
-    // Modify date state variables whenever called (timer-linked. )
+    // Modify date state variables whenever called (timer-linked.)
     App.prototype.updateTime = function () {
         var date = new Date();
         var time = date.toLocaleTimeString(navigator.language, {
@@ -29894,6 +29937,67 @@ var App = /** @class */ (function (_super) {
             currentDayMonthYear: currentDayMonthYear,
         });
     };
+    // Modify weather state variables whenever called (timer-linked)
+    App.prototype.updateWeather = function () {
+        return __awaiter(this, void 0, void 0, function () {
+            var apiResponse, startTime, endTime, timeDiff, error_1, receivedData, weatherMain, weatherDesc, mainTemp, mainFeels_like, mainTemp_min, mainTemp_max, mainPressure, mainHumidity, visibility, windSpeed, windDeg, dt, sysSunrise, sysSunset, currentWeatherMain, currentWeatherMinMax, currentWeatherFeelsLike;
+            return __generator(this, function (_a) {
+                switch (_a.label) {
+                    case 0:
+                        apiResponse = null;
+                        _a.label = 1;
+                    case 1:
+                        _a.trys.push([1, 3, , 4]);
+                        startTime = new Date();
+                        return [4 /*yield*/, fetch("http://api.openweathermap.org/data/2.5/weather?zip=" + openweathermapZipCode + "&units=imperial&appid=" + openweathermapApiKey)];
+                    case 2:
+                        apiResponse = _a.sent();
+                        endTime = new Date();
+                        timeDiff = endTime - startTime;
+                        console.log("DEBUG: Open Weather Map API call returned in " + timeDiff / 1000 + " seconds.");
+                        return [3 /*break*/, 4];
+                    case 3:
+                        error_1 = _a.sent();
+                        console.log("ERROR: Open Weather Map API call failed!");
+                        return [3 /*break*/, 4];
+                    case 4:
+                        if (!(apiResponse.status == 200)) return [3 /*break*/, 6];
+                        return [4 /*yield*/, apiResponse.json()];
+                    case 5:
+                        receivedData = _a.sent();
+                        console.log("DEBUG: Received Open Weather Map API data:");
+                        console.log(receivedData);
+                        weatherMain = receivedData.weather[0].main;
+                        weatherDesc = receivedData.weather[0].description;
+                        mainTemp = receivedData.main.temp;
+                        mainFeels_like = receivedData.main.feels_like;
+                        mainTemp_min = receivedData.main.temp_min;
+                        mainTemp_max = receivedData.main.temp_max;
+                        mainPressure = receivedData.main.pressure;
+                        mainHumidity = receivedData.main.humidity;
+                        visibility = receivedData.visibility;
+                        windSpeed = receivedData.wind.speed;
+                        windDeg = receivedData.wind.deg;
+                        dt = receivedData.dt;
+                        sysSunrise = receivedData.sys.sunrise;
+                        sysSunset = receivedData.sys.sunset;
+                        currentWeatherMain = parseInt(mainTemp).toFixed(0) + " F - " + weatherMain;
+                        currentWeatherMinMax = parseInt(mainTemp_min).toFixed(0) + " F | " + parseInt(mainTemp_max).toFixed(0) + " F";
+                        currentWeatherFeelsLike = "Feels Like: " + parseInt(mainFeels_like).toFixed(0) + " F";
+                        this.setState({
+                            currentWeatherMain: currentWeatherMain,
+                            currentWeatherMinMax: currentWeatherMinMax,
+                            currentWeatherFeelsLike: currentWeatherFeelsLike,
+                        });
+                        return [3 /*break*/, 7];
+                    case 6:
+                        console.log("WARNING: Open Weather Map API call returned with status " + apiResponse.status + ".");
+                        _a.label = 7;
+                    case 7: return [2 /*return*/];
+                }
+            });
+        });
+    };
     App.prototype.moduleLightingBedroom = function () {
         console.log("TODO: Bedroom lighting module activated.");
     };
@@ -29905,9 +30009,12 @@ var App = /** @class */ (function (_super) {
     };
     // Executed only once upon startup.
     App.prototype.componentDidMount = function () {
-        // Start the clock and the interval to update it. 
+        // Start the clock and the interval to update it every second.
         this.updateTime();
         this.updateTimeInverval = setInterval(this.updateTime, 1000);
+        // Query the weather and start the interval to update it (every 60 minutes).
+        this.updateWeather();
+        this.updateWeatherInterval = setInterval(this.updateWeather, 3600000);
     };
     // Executed upon close.
     App.prototype.componentWillUnmount = function () {
@@ -29915,14 +30022,17 @@ var App = /** @class */ (function (_super) {
     };
     App.prototype.render = function () {
         return (React.createElement("div", null,
-            React.createElement("div", { id: "app-location" }, "Location."),
+            React.createElement("div", { id: "app-location" }, "Santa Clara, CA"),
             React.createElement("div", { id: "app-clock" },
                 React.createElement("div", { id: "app-clock-time" },
                     this.state.currentHoursMinutes,
                     React.createElement("span", { id: "app-clock-time-seconds" }, this.state.currentSeconds),
                     React.createElement("span", { id: "app-clock-time-ampm" }, this.state.currentAmPm)),
                 React.createElement("div", { id: "app-clock-date" }, this.state.currentDayMonthYear)),
-            React.createElement("div", { id: "app-weather" }, "Weather."),
+            React.createElement("div", { id: "app-weather" },
+                React.createElement("div", { id: "app-weather-main" }, this.state.currentWeatherMain),
+                React.createElement("div", { id: "app-weather-minMax" }, this.state.currentWeatherMinMax),
+                React.createElement("div", { id: "app-weather-feelsLike" }, this.state.currentWeatherFeelsLike)),
             React.createElement("div", { id: "app-modules" },
                 React.createElement("button", { onClick: this.moduleLightingBedroom }, "Bedroom Light"),
                 React.createElement("button", { onClick: this.moduleCurtainsBedroom }, "Bedroom Curtains"),
