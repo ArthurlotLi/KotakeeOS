ddc986828b71815460e9cbde4f288c71ff0dac5c
arthur <ali64@ucsc.edu>
2021-05-30 11:54:05 -0700

    Implement lower level module functionality

--- software/server.js
+++ software/server.js
@@ -52,6 +52,35 @@ class Home {
     this.rooms = roomsDict
     this.weatherData = weatherData;
   }
+
+  // Returns various general data.
+  homeStatus(){
+    var status = {
+      modulesCount = null,
+      weatherData = null,
+    }
+
+    // Get total modules. 
+    var modulesCount = 0;
+    for(var room in this.rooms){
+      if(rooms.hasOwnProperty(room)){
+        modulesCount = modulesCount + room.modulesCount;
+      }
+    }
+
+    status.modulesCount = modulesCount;
+    status.weatherData = this.weatherData;
+    console.log("[DEBUG] Retreived weather data: " + JSON.stringify(status));
+    return status;
+  }
+
+  // Returns a room object given ID. 
+  getRoom(roomId){
+    if(roomId in this.roomsDict){
+      return this.roomsDict[roomId];
+    }
+    return null;
+  }
 }
 
 // Each room contains room enum as well as an array of modules. 
@@ -62,7 +91,8 @@ class Room {
     // by moduleId. 
     var actionsDict = {};
     var modulesDict = {};
-    for(var i = 0; i < modules.length; i++){
+    var modulesCount = modules.length;
+    for(var i = 0; i < modulesCount; i++){
       // Iterate through all modules in room and save all seen actions.
       // Link each action to the module object itself for easy access.
       var module = modules[i];
@@ -74,7 +104,27 @@ class Room {
     }
     this.actionsDict = actionsDict;
     this.modulesDict = modulesDict;
-    console.log("[DEBUG] Created module with actionsDict: " + JSON.stringify(actionsDict) + " and modulesDict: " + JSON.stringify(modulesDict));
+    this.modulesCount = modulesCount;
+    console.log("[DEBUG] Created room id " + roomId+ " with the following info:\nactionsDict " + JSON.stringify(actionsDict) + "\nmodulesDict " + JSON.stringify(modulesDict));
+  }
+
+  // Given action Id and toState, execute Module code if found in dict
+  // AND if the state is not what is currently stored. Otherwise ignore.
+  // Returns true or false depending on execution status. 
+  actionToggle(actionId, toState){
+    if(actionId in this.actionsDict){
+      var moduleId = this.actionsDict[actionId];
+      if(moduleId in this.modulesDict){
+        var module = this.modulesDict[moduleId];
+
+        return module.actionToggle(actionid, toState);
+      }
+      else 
+        console.log("[ERROR] actionToggle failed! actionId " + actionId + " WAS found, but the saved moduleId "+ moduleId +" does not exist in room " + this.roomId + ".");
+    }
+    else 
+      console.log("[ERROR] actionToggle failed! actionId " + actionId + " does not exist in room " + this.roomId + ".");
+    return false;
   }
 }
 
@@ -82,13 +132,69 @@ class Room {
 class Module {
   constructor(moduleId, actions, ipAddress){
     this.moduleId = moduleId;
-    this.actions = actions;
-    var newStates = [];
-    for(var i = 0; i < actions.length; i++){
-      newStates.push(0);
-    }
-    this.states = newStates;
     this.ipAddress = ipAddress;
+    this.actions = actions;
+    // Create a dictionary of states indexed by actionId. 
+    var statesDict = {};
+    for(var i = 0; i < actions.length; i++){
+      statesDict[action[i]] = 0;
+    }
+    this.statesDict = statesDict;
+  }
+
+  // Given an actionId, return current state. Returns null if actionId
+  // does not exist for this module (not implemented).
+  getActionState(actionId){
+    if(this.actions.includes(actionId)){
+      if(actionId in this.statesDict){
+        return this.statesDict.actionId; 
+      }
+      else 
+        console.log("[ERROR] getActionState failed! actionId " + actionId + " is implemented, but there is no statesDict entry for module" + this.moduleId + ".");
+    }
+    else 
+      console.log("[ERROR] getActionState failed! actionId " + actionId + " is not implemented for module " + this.moduleId + ".");
+    return null;
+  }
+
+  // Given actionId and toState, sends instruction to module to change 
+  // the state (if it is a valid new state). Returns true if action was
+  // successful, false if something went wrong (i.e. given state is
+  // actually current)
+  actionToggle(actionId, toState){
+    var stateRetVal = this.getActionState(actionId);
+    if(stateRetVal){
+      if(stateRetVal != toState){ 
+        // Verified that the action is correct. Execute the action.
+        return await requestGetStateToggle(actionId, toState);
+      }
+      else
+        console.log("[WARNING] Provided toState \'" + toState + "\' for " + actionId + " conflicts with existing state \'"+stateRetVal+"\' for module " + this.moduleId + ".");
+    }
+    return false;
+  }
+
+  // Sends a request to the arduino to change to a new state. Returns
+  // true if the action was succesfully received (200).
+  async requestGetStateToggle(actionId, toState){
+    var apiResponse = null;
+    var startTime, endTime; // We report in debug the api time.
+    try{
+      startTime = new Date();
+      apiResponse = await fetch('http://' + this.ipAddress + '/stateToggle/' + actionId + '/' + toState); 
+      endTime = new Date();
+      var timeDiff = endTime - startTime;
+      console.log("[DEBUG] requestGetStateToggle (module " +this.moduleId+ ") returned in " + timeDiff/1000 + " seconds.");
+    }
+    catch(error){
+      console.log("[ERROR] requestGetStateToggle (module " +this.moduleId+ ") failed!");
+    }
+    if(apiResponse.status == 200){
+      // Executed successfully!
+      return true;
+    }
+    console.log("[WARNING] requestGetStateToggle (module " +this.moduleId+ ") returned with status " + apiResponse.status + ".");
+    return false;
   }
 }
 
@@ -119,6 +225,8 @@ const home = new Home(homeRooms, {}); // TODO add weather data.
 
 // Create the app
 const app = express();
+
+console.log("Testing bedroom actionToggle(50). " + bedroom.actionToggle(50));
 
 /*
   Web Application logic
