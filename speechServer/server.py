e41b48f8d2409e0cf11822b8cc82d3c574862282
arthur <ali64@ucsc.edu>
2021-06-12 14:12:00 -0700

    Begin directory for speech recognition application server

---
+++ speechServer/server.py
@@ -0,0 +1,19 @@
+#
+# sever.py
+#
+# Root program for speech recognition application server designed
+# to interface with the kotakeeOS Web Server. This sends requests
+# to activate-deactivate modules just like any other regular 
+# web application. 
+#
+
+# Application constants.
+webServerIpAddress = "192.168.0.197:8080"
+
+def runApplicationServer():
+  print("Initializing kotakeeOS speech application server...")
+
+  
+
+if __name__ == "__main__":
+  runApplicationServer()
