9042169fe1ebedd7542ca585072b452f1ca3c360
arthur <ali64@ucsc.edu>
2021-06-13 14:22:47 -0700

    Add homeStatus and actionStates parsing whenever the hotword has been detected. Utilize this information to make on or off specification optional.

--- speechServer/server.py
+++ speechServer/server.py
@@ -20,12 +20,14 @@ import time
 
 import threading
 
+import json
+
 # Application constants.
 webServerIpAddress = "http://192.168.0.197:8080"
 
 #hotWord = "iris"
 #hotWord = "california" # Triggers activation of google query. 
-hotWords = ["california", "america", "initial", "system", "iris"]
+hotWords = ["california", "america", "initial", "initialize", "system", "iris"]
 
 cancelWords = ["stop", "cancel", "go away", "quit", "no thanks"] # stops google query.
 hotWordReceiptPrompt = "Yes?"
@@ -41,6 +43,12 @@ engine = pyttsx3.init()
 
 # Kill swith
 stopServer = False
+
+# JSON objects to query for.
+actionStates = None
+homeStatus = None
+actionStatesLastUpdate = 0
+homeStatusLastUpdate = 0
 
 def runApplicationServer():
   print("Initializing kotakeeOS speech application server...")
@@ -94,6 +102,13 @@ def listenForHotWord():
 # Uses far more intelligent google API to parse a command. 
 def listenForCommand():
   successfulCommand = False
+
+  # When the hotword has been said, kick off a thread
+  # to query the server for the latest homeStatus and
+  # states. This should be fast enough so that it's always
+  # done by the time the user says the command. 
+  executeQueryServerThread()
+
   # Try three times, or until user cancels, or command was
   # executed.
   for i in range(3): 
@@ -153,19 +168,60 @@ def speakText(command):
   engine.say(command)
   engine.runAndWait()
 
+# Non-blocking query to fill status objects.
+def executeQueryServerThread():
+  queryActionStatesThread = threading.Thread(target=queryActionStates, daemon=True).start()
+  queryHomeStatusThread = threading.Thread(target=queryHomeStatus, daemon=True).start()
+
+# Queries server for states of all modules. 
+def queryActionStates():
+  global actionStatesLastUpdate
+  global actionStates
+  query = webServerIpAddress + "/actionStates/" + str(actionStatesLastUpdate)
+  print("[DEBUG] Querying server: " + query)
+  response = requests.get(query)
+  if(response.status_code == 200):
+    actionStates = json.loads(response.text)
+    actionStatesLastUpdate = actionStates['lastUpdate']
+    print("[DEBUG] Action States request received successfully. actionStatesLastUpdate is now: " + str(actionStatesLastUpdate))
+    #print(str(actionStates))
+  elif(response.status_code != 204):
+    print("[WARNING] Server rejected request with status code " + str(response.status_code) + ".")
+
+# Queries server for misc non-module information
+def queryHomeStatus():
+  global homeStatusLastUpdate
+  global homeStatus
+  query = webServerIpAddress + "/homeStatus/" + str(homeStatusLastUpdate)
+  print("[DEBUG] Querying server: " + query)
+  response = requests.get(query)
+  if(response.status_code == 200):
+    homeStatus = json.loads(response.text)
+    homeStatusLastUpdate = homeStatus['lastUpdate']
+    print("[DEBUG] Home Status request received successfully. homeStatusLastUpdate is now: " + str(homeStatusLastUpdate))
+    #print(str(homeStatus))
+  elif(response.status_code != 204):
+    print("[WARNING] Server rejected request with status code " + str(response.status_code) + ".")
+
 # Given a queried command from the google text recognition
 # API, parse and execute accordingly.
 def parseAndExecuteCommand(command):
   global stopServer
+  global homeStatus
+  global actionStates
   # Ex) http://192.168.0.197:8080/moduleToggle/1/50/1
   queries = []
 
-  # The silly command
   if ("good night" in command or "freeze all motor functions" in command or "goodnight" in command):
     executeTextThread(stopServerPrompt)
     time.sleep(5) # Enough time to allow the speech prompt to complete. 
     stopServer = True
     return True
+  elif("weather" in command or "like outside" in command or "how hot" in command or "how cold" in command):
+    if(homeStatus is not None):
+      weatherString = "It is currently " + str(int(homeStatus["weatherData"]["main"]["temp"])) + " degrees Fahrenheit, " +str(homeStatus["weatherData"]["weather"][0]["main"]) + ", with a maximum of " + str(int(homeStatus["weatherData"]["main"]["temp_max"])) + " and a minimum of " + str(int(homeStatus["weatherData"]["main"]["temp_min"])) + ". Humidity is " +  str(homeStatus["weatherData"]["main"]["humidity"]) + " percent."
+      executeTextThread(weatherString)
+      return True
   elif("everything" in command):
     if("off" in command):
       queries.append(webServerIpAddress + "/moduleToggle/1/50/0")
@@ -185,30 +241,65 @@ def parseAndExecuteCommand(command):
         queries.append(webServerIpAddress + "/moduleToggle/1/50/0") # query off first, because on is in one. 
       elif("on" in command):
         queries.append(webServerIpAddress + "/moduleToggle/1/50/1")
+      else:
+        #No on or off specified. Check queried information. 
+        if(actionStates is not None):
+          if(actionStates["1"]["50"] == 1):
+            queries.append(webServerIpAddress + "/moduleToggle/1/50/0")
+          else:
+            queries.append(webServerIpAddress + "/moduleToggle/1/50/1")
 
     if("living" in command and ("light" in command or "lights" in command or "lamp" in command)):
       if("off" in command):
         queries.append(webServerIpAddress + "/moduleToggle/2/50/0")
       elif("on" in command):
         queries.append(webServerIpAddress + "/moduleToggle/2/50/1")
+      else:
+        #No on or off specified. Check queried information. 
+        if(actionStates is not None):
+          if(actionStates["2"]["50"] == 1):
+            queries.append(webServerIpAddress + "/moduleToggle/2/50/0")
+          else:
+            queries.append(webServerIpAddress + "/moduleToggle/2/50/1")
     
     if("speaker" in command or "soundbar" in command or ("sound" in command and "bar" in command)):
       if("off" in command):
         queries.append(webServerIpAddress + "/moduleToggle/2/250/10")
       elif("on" in command):
         queries.append(webServerIpAddress + "/moduleToggle/2/250/12")
+      else:
+        #No on or off specified. Check queried information. 
+        if(actionStates is not None):
+          if(actionStates["2"]["250"] == 12):
+            queries.append(webServerIpAddress + "/moduleToggle/2/250/10")
+          else:
+            queries.append(webServerIpAddress + "/moduleToggle/2/250/12")
     
     if("ceiling" in command and ("light" in command or "lights" in command or "lamp" in command)):
       if("off" in command):
         queries.append(webServerIpAddress + "/moduleToggle/2/251/10")
       elif("on" in command):
         queries.append(webServerIpAddress + "/moduleToggle/2/251/12")
+      else:
+        #No on or off specified. Check queried information. 
+        if(actionStates is not None):
+          if(actionStates["2"]["251"] == 12):
+            queries.append(webServerIpAddress + "/moduleToggle/2/251/10")
+          else:
+            queries.append(webServerIpAddress + "/moduleToggle/2/251/12")
 
     if("kitchen" in command and ("light" in command or "lights" in command or "lamp" in command)):
       if("off" in command):
         queries.append(webServerIpAddress + "/moduleToggle/2/350/20")
       elif("on" in command):
         queries.append(webServerIpAddress + "/moduleToggle/2/350/22")
+      else:
+        #No on or off specified. Check queried information. 
+        if(actionStates is not None):
+          if(actionStates["2"]["350"] == 22):
+            queries.append(webServerIpAddress + "/moduleToggle/2/350/20")
+          else:
+            queries.append(webServerIpAddress + "/moduleToggle/2/350/22")
 
   if len(queries) > 0:
     # We have received a valid command. Query the server. 
@@ -222,7 +313,7 @@ def parseAndExecuteCommand(command):
     executeTextThread(successfulCommandPrompt)
     return True
   else:
-    executeTextThread(failedCommandPrompt)
+    #executeTextThread(failedCommandPrompt)
     print("[DEBUG] No valid command was received.")
     return False
 
