934329d17460509471bc3d1416d52838a3784f4c
arthur <ali64@ucsc.edu>
2021-06-13 18:00:35 -0700

    Disable youre welcome parsing. It's creepy

--- speechServer/server.py
+++ speechServer/server.py
@@ -91,9 +91,11 @@ def listenForHotWord():
       if stopServer is not True and any(x in recognizedText for x in hotWords):
         print("[DEBUG] Hotword recognized!")
         listenForCommand()
-      elif stopServer is not True and "thank you" in recognizedText or "thanks" in recognizedText:
+      # Disabled, because a bot randomly saying "You're welcome"
+      # behind my back is freaking me out. 
+      #elif stopServer is not True and "thank you" in recognizedText or "thanks" in recognizedText:
         # For fun. 
-        executeTextThread("You're welcome.")
+        #executeTextThread("You're welcome.")
 
   except sr.RequestError as e:
     print("[ERROR] Could not request results from speech_recognition; {0}.format(e)")
