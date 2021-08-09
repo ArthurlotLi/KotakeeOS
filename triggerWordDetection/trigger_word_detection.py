2d67f8c1e15e75393aadb4f02a6ea794e6f7baba
arthur <ali64@ucsc.edu>
2021-08-09 07:29:42 -0700

    PUsh up results of two EC2 learning instances + change epochs to 150.

--- triggerWordDetection/trigger_word_detection.py
+++ triggerWordDetection/trigger_word_detection.py
@@ -361,7 +361,7 @@ def train_model(X, Y):
   # Tuning parameters that can be tweaked. 
   learning_rate = 0.0001 # A healthy learning rate. 
   loss_function = 'binary_crossentropy'
-  epochs = 250 # Around this point is where we hit 97% accuracy, so let's try stopping here. 
+  epochs = 150 # Around this point is where we hit 97% accuracy, so let's try stopping here. 
   batch_size=32 # In general, 32 is a good starting point, then try 64, 128, 256. Smaller but not too small is optimal for accuracy. 
   validation_split = 0.2
   rlr_patience = 5
