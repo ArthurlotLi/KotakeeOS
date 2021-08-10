c34f6f38883f06279b8f343a9702b80ddf0950fd
arthur <ali64@ucsc.edu>
2021-08-10 14:36:11 -0700

    Dump new dataset with refined backgrounds and activates.

---
+++ triggerWordDetection/test_model.py
@@ -0,0 +1,57 @@
+# Small utility program to run a specified model iteration
+# against dev set to see accuracy. 
+
+# TODO: I imported everythign because I"m lazy. Please remove unecessary imports later. 
+#import matplotlib.pyplot as plt
+#from scipy.io import wavfile
+import os
+#from pydub import AudioSegment
+
+import argparse
+
+import numpy as np
+import tensorflow as tf
+from tensorflow.keras.models import Model, load_model
+
+def test_model(iternum):
+  models_path = './models'
+
+  # Load pretrained model
+  if(int(iternum) <= 0):
+    # If we're using 0 or less, we're using the pretrained model.
+    # If so, we need to adjust becuase it was trained in tf1 and
+    # we're using tf2. 
+    tf.compat.v1.disable_v2_behavior()
+    model = tf.compat.v1.keras.models.load_model(models_path + '/tr_model_'+str(iternum) +'.h5')
+  else:
+    # Load our model. 
+    model = load_model(models_path + '/tr_model_'+str(iternum) +'.h5')
+
+  if(model is None):
+    print('[ERROR] Unable to load trigger word detection model. Path: '+models_path +'/tr_model_'+str(iternum) +'.h5. Stopping...')
+    return
+  
+  try:
+    print("[INFO] Loading dev dataset file ./XY_dev/X_dev.npy...")
+    X_dev = np.load("./XY_dev/X_dev.npy")
+    print("[INFO] Loading existing dataset file ./XY_dev/Y_dev.npy...")
+    Y_dev = np.load("./XY_dev/Y_dev.npy")
+    print("[DEBUG] X_dev.shape is:", X_dev.shape)  
+    print("[DEBUG] Y_dev.shape is:", Y_dev.shape) 
+
+    loss, acc = model.evaluate(X_dev, Y_dev)
+    print("[INFO] Dev set accuracy is: ", acc) 
+    if(float(acc) <= 0.94 and float(acc) >= 0.90):
+      #print("[INFO] ...Unfortunately this one doesn't look too good. The model underfit and likely trained on 0s.")
+      print("[INFO] When checked against clips of absolutely no positives, looks okay (should've given us all 0s.)")
+  except:
+    print("[WARN] Error loading X_dev and/or Y/dev.")
+
+if __name__ == "__main__":
+  parser = argparse.ArgumentParser()
+  parser.add_argument('iternum')
+  args = parser.parse_args()
+
+  iternum = int(args.iternum)
+
+  test_model(iternum)
--- triggerWordDetection/trigger_word_detection.py
+++ triggerWordDetection/trigger_word_detection.py
@@ -464,14 +464,14 @@ def define_model(input_shape):
 
     # Step 2: First GRU Layer (≈4 lines)
     X = GRU(units = 128, return_sequences = True)(X) # GRU (use 128 units and return the sequences)
-    X = Dropout(0.5)(X)                                 # dropout (use 0.8)
+    X = Dropout(0.8)(X)                                 # dropout (use 0.8)
     X = BatchNormalization()(X)                                 # Batch normalization
     
     # Step 3: Second GRU Layer (≈4 lines)
     X = GRU(units = 128, return_sequences = True)(X)   # GRU (use 128 units and return the sequences)
-    X = Dropout(0.5)(X)                                 # dropout (use 0.8)
+    X = Dropout(0.8)(X)                                 # dropout (use 0.8)
     X = BatchNormalization()(X)                                  # Batch normalization
-    X = Dropout(0.5)(X)                                  # dropout (use 0.8)
+    X = Dropout(0.8)(X)                                  # dropout (use 0.8)
     
     # Step 4: Time-distributed dense layer (≈1 line)
     X = TimeDistributed(Dense(1, activation = "sigmoid"))(X) # time distributed  (sigmoid)
