973b5a4fd5a9dd8156dbeb3b5c5a7313946ed1e3
Arthur <Artieli007@gmail.com>
2021-12-25 15:55:22 -0800

    Fix routines that add a query other than single items.

--- speechServer/commandParsing.py
+++ speechServer/commandParsing.py
@@ -556,18 +556,18 @@ class CommandParser:
         if("living" in command and ("led" in command or "party" in command or "rgb" in command)):
           queries.append(self.generateQuery(command, 2, 1000, 107, 100))
 
-        if len(queries) > 0:
-          # We have received a valid command. Query the server. 
-          for query in queries:
-            print("[DEBUG] Sending query: " + query)
-            response = requests.get(query)
-            if(response.status_code == 200):
-              print("[DEBUG] Request received successfully.")
-            else:
-              print("[WARNING] Server rejected request with status code " + str(response.status_code) + ".")
-          if(confirmationPrompt is not None and confirmationPrompt != ""):
-            self.executeTextThread(confirmationPrompt)
-          valid_command = True
+      if len(queries) > 0:
+        # We have received a valid command. Query the server. 
+        for query in queries:
+          print("[DEBUG] Sending query: " + query)
+          response = requests.get(query)
+          if(response.status_code == 200):
+            print("[DEBUG] Request received successfully.")
+          else:
+            print("[WARNING] Server rejected request with status code " + str(response.status_code) + ".")
+        if(confirmationPrompt is not None and confirmationPrompt != ""):
+          self.executeTextThread(confirmationPrompt)
+        valid_command = True
 
     if valid_command != True:
       print("[DEBUG] No valid command was received.")
