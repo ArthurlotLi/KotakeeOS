3f140919c1082020088c9039af6426a8d54e23b5
Arthur <Artieli007@gmail.com>
2021-12-28 15:36:59 -0800

    Attempt to utilize sys.path modification for dynamic class import.

--- speech_server/module_active.py
+++ speech_server/module_active.py
@@ -17,6 +17,7 @@
 # development. 
 
 import json
+import sys
 
 class ModuleActive:
   # Indicates successful initialization. If at any point an exception
@@ -158,16 +159,29 @@ class ModuleActive:
     self.valid_module = True
     print("[DEBUG] Successfully loaded class " + str(self.class_name) + " from '" + str(self.class_location) + "'.")
       
-  # Dynamic class import 
+  # Dynamic class import. Changes sys.path to navigate directories. 
+  # Expects module_name Ex) ./home_automation/home_automation
+  # and class_name Ex) HomeAutomation
   def load_class(self,  module_name, class_name):
     module = None
     imported_class = None
+    module_file_name = None
+
+    # Ex) ./home_automation - split by last slash. 
+    # Don't bother if the original file is not within a subdirectory.
+    split_module_name = module_name.rsplit("/", 1)
+    module_folder_path = split_module_name[0]
+    if(module_folder_path != "." and len(split_module_name) > 1):
+      sys.path.append(module_folder_path)
+      module_file_name = split_module_name[1]
+    else:
+      module_file_name = module_name.replace("./", "")
 
     # Fetch the module first.
     try:
-      module = __import__(module_name)
+      module = __import__(module_file_name)
     except:
-      print("[ERROR] Failed to import module " + module_name + ".")
+      print("[ERROR] Failed to import module " + module_file_name + " from subdirectory '" + module_folder_path + "'.")
       return None
 
     # Return the class. 
