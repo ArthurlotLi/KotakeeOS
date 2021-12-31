235c882420cbc5f9de0d85c8b9657c535ae83677
Arthur <Artieli007@gmail.com>
2021-12-30 20:08:59 -0800

    I have NO idea why that's a tuple. Let's try this.

--- speech_server/simple_utilities/timer_utility/timer_utility.py
+++ speech_server/simple_utilities/timer_utility/timer_utility.py
@@ -8,14 +8,15 @@
 class TimerUtility:
   speech_speak = None
 
+  # When initializing from a thread, be warned that arguments
+  # come through as tuples (Ex) speech_speak = (speech_speak.SpeechSpeak,))
   def __init__(self, speech_speak):
-    self.speech_speak = speech_speak
+    self.speech_speak = speech_speak[0]
 
   # Standard routine triggered when the event time is triggered
   # by the passive interaction thread. 
   def activate_event(self):
     print("[INFO] Timer event triggered. Executing timer and text.")
-    #self.speech_speak.execute_timer()
-    print(self.speech_speak)
+    self.speech_speak.execute_timer()
     self.speech_speak.speak_text("Timer finished.")
     print("[DEBUG] Timer event complete.")
