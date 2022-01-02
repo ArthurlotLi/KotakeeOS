069b88afcecbf554ccbe812a84716c0e32976544
Arthur <Artieli007@gmail.com>
2022-01-01 17:05:18 -0800

    Implement thread-safe queue into speech_listen.

--- speech_server/speech_listen.py
+++ speech_server/speech_listen.py
@@ -25,6 +25,10 @@ class SpeechListen:
   # that a separate thread is using the microphone. 
   speech_listen_active = False
 
+  # An list of timestamps that serve as unique IDs for events that
+  # must be queued. 
+  speech_listen_queued = []
+
   # Configuration parameters
   default_pause_threshold = 1.0
   default_max_response_attempts = 1
@@ -51,11 +55,34 @@ class SpeechListen:
   # the microphone - useful for other threads that need to wait for
   # hotword_tirgger_word to release the mic stream. Delay is in 
   # seconds (float).
+  #
+  # In the edge case of multiple threads attempting to listen at the
+  # same time, this function blocks the calling thread until the
+  # active interaction completes. 
   def listen_response(self, prompt = None, indicate_led = True, execute_chime = False, pause_threshold = None, max_response_attempts = None, response_timeout = None, response_phrase_timeout = None, ambient_noise_duration = None, start_delay = None):
     user_response_text = None
 
+    if len(self.speech_listen_queued) > 0:
+      # Another thread is currently using this function. Insert
+      # a ticket and wait for your name to be called. Your unique
+      # id is the current system time down to the ms. Events coming
+      # in on the same ms is an edge case we're accepting. 
+      timestamp = round(time.time() * 1000)
+      self.speech_listen_queued.append(timestamp)
+      print("[DEBUG] Blocking thread given that Speech Listen is currently active.")
+      while self.speech_listen_queued[0] != timestamp:
+        # Block the thread and wait until you are first in line 
+        # and the function is no longer in use. 
+        time.sleep(0.10)
+      # Clear yourself from the list. 
+      print("[DEBUG] Thread block lifted given that Speech Listen is no longer active.")
+    else:
+      # Insert yourself into the empty speech_listen_queued. Since
+      # there's only one of us, there's no need for a unique ID. 
+      self.speech_listen_queued.append("-1") 
+
     # Inidicate that we're active. Tells the hotword parsing to 
-    # stop listening if we're being called from a separate thread. 
+    # stop listening if we're being called from a separate thread.
     self.speech_listen_active = True
 
     if start_delay is not None:
@@ -123,6 +150,11 @@ class SpeechListen:
         self.web_server_status.query_speech_server_module_toggle(self.led_state_off, self.led_room_id, self.led_action_id)
   
     # All done. Disable the flag and let the hotword parsing continue.
-    self.speech_listen_active = False
+    # Note however that if we were not the only one in the queue someone
+    # else is going to be using this function right after, so don't
+    # let hotword parsing continue and just exit. 
+    del self.speech_listen_queued[0] 
+    if len(self.speech_listen_queued) == 0:
+      self.speech_listen_active = False
 
     return user_response_text
