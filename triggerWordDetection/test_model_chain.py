1c65e4136220fda45de61edb59d88a0daa19dbe0
Arthur <Artieli007@gmail.com>
2022-02-01 07:09:21 -0800

    Implement parallel processing with minibatch processes for testing.

--- triggerWordDetection/test_model_chain.py
+++ triggerWordDetection/test_model_chain.py
@@ -4,6 +4,7 @@
 # Tests all models currently present in a specified directory
 # given the class defined in test_model. 
 
+from struct import pack_into
 from test_model import TestModel
  
 import os
@@ -20,6 +21,8 @@ class TestModelChain:
   chain_test_results = {}
   chain_test_results_acc_map = {}
   test_model_location = None
+
+  minibatch_size = 8
 
   def __init__(self, location):
     self.test_model_location = location
@@ -43,50 +46,79 @@ class TestModelChain:
       print("[WARN] Error loading X_dev and/or Y/dev.")
       return -1
 
-    for filename in os.listdir(self.test_model_location):
+    minibatch = []
+    
+    files = os.listdir(self.test_model_location)
+
+    for j in range(0, len(files)):
+      filename = files[j]
       if filename.endswith("h5"):
+        minibatch.append(filename)
+      # If a minibatch has been filled OR we're at the end of files. 
+      if len(minibatch) == self.minibatch_size or j == len(files)-1:
         try:
-          print("\n[INFO] Processing model " + str(filename) + ".")
-          
-          # Execute test as a separate process. Use a queue to
-          # obtain results. 
-          ret_dict = {"acc":None}
+          print("[INFO] Processing minibatch: " + str(minibatch))
+          ret_dict = {"acc0":None,"acc1":None,"acc2":None,"acc3":None}
           queue = multiprocessing.Queue()
           queue.put(ret_dict)
 
-          print("[INFO] Executing new process...")
-          p = multiprocessing.Process(target=self.test_model_worker, args=(queue, self.test_model_location + "/" + filename, X_dev, Y_dev))
-          p.start()
-          p.join()
+          minibatch_processes = {}
+
+          for i in range(0, len(minibatch)):
+            file = minibatch[i]
+            print("[INFO] Creating new subprocess for model " + str(file) + ".")
+            
+            # Execute test as a separate process. Use a queue to
+            # obtain results. 
+            p = multiprocessing.Process(target=self.test_model_worker, args=(queue, self.test_model_location + "/" + file, X_dev, Y_dev, "acc" + str(i)))
+            minibatch_processes[i] = (p, file)
+
+          # After all of the processes have been created. Kick off in parallel.
+          for item in minibatch_processes:
+            tuple = minibatch_processes[item]
+            print("\n\n[INFO] Executing new process for model " + tuple[1] + ".\n")
+            tuple[0].start()
+
+          # Now wait for all of them.
+          for p in minibatch_processes:
+            tuple[0].join()
           ret_dict_result = queue.get()
-          print("\n[INFO] Process complete; result: ")
-          print(ret_dict_result)
-          acc = ret_dict_result["acc"]
-        
-          if acc is None:
-            print("[ERROR] Failed to process model " + str(filename) + "!")
-            self.chain_test_results[filename_uid] = "00.00000000 - " + str(filename) + " TEST FAILED!\n"
-            self.chain_test_results_acc_map[-1] = filename_uid
-          else:
-            print("[INFO] Model " + str(filename) + " processing complete.")
-            self.chain_test_results[filename_uid] = "%.8f - " % (acc*100) + str(filename) + "\n"
-            # If a model of that exact accuracy exists already, append
-            # a tiny number to it until it's unique. 
-            if acc in self.chain_test_results_acc_map:
-              sorting_acc = None
-              while sorting_acc is None:
-                acc = acc + 0.000000000000001 # Acc has 15 decimal precision. Append by 1 to break ties.
-                if acc not in self.chain_test_results_acc_map:
-                  sorting_acc = acc
-              self.chain_test_results_acc_map[sorting_acc] = filename_uid
-            else:
-              self.chain_test_results_acc_map[acc] = filename_uid
-        except:
+          print("\n[INFO] Processes complete; results:")
+          print(ret_dict_result) 
+          print("")
+          for item in ret_dict_result:
+            item_identifier = int(item.replace("acc",""))
+            if item_identifier in minibatch_processes:
+              filename = minibatch_processes[int(item.replace("acc",""))][1]
+              acc = ret_dict_result[item]
+            
+              if acc is None:
+                print("[WARN] Received empty acc!")
+                self.chain_test_results[filename_uid] = "00.00000000 - " + str(filename) + " TEST FAILED!\n"
+                self.chain_test_results_acc_map[-1] = filename_uid
+              else:
+                self.chain_test_results[filename_uid] = "%.8f - " % (acc*100) + str(filename) + "\n"
+                # If a model of that exact accuracy exists already, append
+                # a tiny number to it until it's unique. 
+                if acc in self.chain_test_results_acc_map:
+                  sorting_acc = None
+                  while sorting_acc is None:
+                    acc = acc + 0.000000000000001 # Acc has 15 decimal precision. Append by 1 to break ties.
+                    if acc not in self.chain_test_results_acc_map:
+                      sorting_acc = acc
+                  self.chain_test_results_acc_map[sorting_acc] = filename_uid
+                else:
+                  self.chain_test_results_acc_map[acc] = filename_uid
+              
+              filename_uid = filename_uid + 1
+            
+          print("\n[INFO] Minibatch: " + str(minibatch) + " processing complete.\n")
+        except Exception as e:
           # Use a try/except so that we still write the remaining stuff 
           # to file in case of a failure or the user cancels the rest.
-          print("[ERROR] Failed to process model " + str(filename) + "!")
-        
-        filename_uid = filename_uid + 1
+          print("[ERROR] Failed to process model " + str(filename) + "! Exception:")
+          print(e)
+        minibatch = []
 
     if(filename_uid == 0):
       print("[WARNING] No models found at location '" + self.test_model_location + "'. Please specify another location with an argument (example: python test_model_chain.py ./model_checkpoints) or move/copy the model(s) accordingly.")
@@ -103,11 +135,11 @@ class TestModelChain:
   # memory of the GPU and not freak out when we train another
   # right after. If the GPU is disabled, this still allows the 
   # memory to be handled properly. 
-  def test_model_worker(self, queue, model_path, X_dev = None, Y_dev = None):
+  def test_model_worker(self, queue, model_path, X_dev = None, Y_dev = None, acc_dict_name = "acc"):
     test_model = TestModel()
     acc = test_model.test_model(model_path = model_path, X_dev = X_dev, Y_dev = Y_dev, use_gpu = self.use_gpu)
     ret_dict = queue.get()
-    ret_dict["acc"] = acc
+    ret_dict[acc_dict_name] = acc
     queue.put(ret_dict)
     
   def write_results(self):
