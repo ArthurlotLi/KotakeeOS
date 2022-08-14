c74f0345e837c7c975990c94531d7ed464df001a
Arthur <Artieli007@gmail.com>
2022-08-14 15:34:37 -0700

    Modified automated lights, yet again.

--- software/server.js
+++ software/server.js
@@ -215,7 +215,7 @@ const bedroom = new Room(rooms.BEDROOM,bedroomModules, bedroomInputActions);
 const livingRoomModules = [module2LR, module3LR, module4LR, module7LR, module6LR];
 const livingRoomInputActionsTimeBounds = {
   // MinHr, MinMin, MaxHr, MaxMin
-  350: [4, 20, 18, 00], // These arrays must be multiples of 4. 
+  350: [1, 20, 16, 00], // These arrays must be multiples of 4. 
 }
 const airConditioningOn = 81; // How hot it must be to turn on the air conditioner. 
 const airConditioningOff = 79; // How hot it must be to turn off the air conditioner. 
@@ -291,8 +291,8 @@ const livingRoom = new Room(rooms.LIVINGROOM,livingRoomModules, livingRoomInputA
 const bathroomModules = [module5BA];
 const bathroomInputActionsTimeBounds = {
   // MinHr, MinMin, MaxHr, MaxMin
-  350: [4, 20, 18, 00], // These arrays must be multiples of 4. 
-  50: [18, 01, 23, 59, 0, 0, 4, 19],
+  350: [1, 20, 16, 00], // These arrays must be multiples of 4. 
+  50: [16, 01, 23, 59, 0, 0, 1, 19],
 }
 const fanOnHumidity = 85;
 const fanOffHumidity = 84; // Not used. 
